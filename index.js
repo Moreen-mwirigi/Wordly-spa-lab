f9d499f0539e158b5c2a3a7962b9211d2c388cde
Moreen-mwirigi <mwirigimoreen.mk@gmail.com>
2026-05-02 01:02:46 +0300

    second

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/<word>'
-let currentWord = ''
-let favorites = JSON.parse(localStorage.getItem('wordly-favorite')) || []
+const apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 
 const themeToggle = document.getElementById('theme-toggle')
 const searchForm = document.getElementById('search-form')
 const wordInput = document.getElementById('input-word')
 const errorMessage = document.getElementById('error-message')
+const errorText = document.getElementById('error-text')
 const loader = document.getElementById('loader')
-const result = document.getElementById('result-section')
-const favorites = document.getElementById('favourite-section')
+const resultSection = document.getElementById('result-section')
+const favoriteSection = document.getElementById('favourites-section')
+const favoritesList = document.getElementById('favorites-list')
+const favoritesEmpty = document.getElementById('favorites-empty')
+
+let currentWord = ''
+let favorites = JSON.parse(localStorage.getItem('wordly-favorite')) || []
 
 document.addEventListener('DOMContentLoaded', () => {
     renderFavorites()
@@ -16,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const word = wordInput.ariaValueMax.trim()
+    const word = wordInput.value.trim()
     if (word) searchWord(word)
 })
 themeToggle.addEventListener('click', toggleTheme)
@@ -27,9 +31,9 @@ async function searchWord(word) {
     hideResults()
 
     try{
-        const search = await fetch(`${apiUrl}${word}`)
-        if (!search.ok) throw new Error('Word not found')
-            const data = await search.json()
+        const res = await fetch(`${apiUrl}${word}`)
+        if (!res.ok) throw new Error('Word not found')
+            const data = await res.json()
         displayResult(data[0])
         currentWord = data[0].word
     } catch (error){
@@ -40,9 +44,23 @@ async function searchWord(word) {
         hideLoader()
     }
 }
+console.log('word element:', document.getElementById('word'))
+console.log('phonetic element:', document.getElementById('phonetic'))
+console.log('audio element:', document.getElementById('audio'))
+console.log('save-btn element:', document.getElementById('save-btn'))
+console.log('meanings element:', document.getElementById('meanings'))
+
+
 function displayResult(data) {
+    document.getElementById('loader')?.classList.add('hidden')
+    document.getElementById('error-message')?.classList.add('hidden')
+    document.getElementById('meanings').innerHTML = ''
+   
+    console.log('1. api data received:', data)
+
     document.getElementById('word').textContent = data.word
     document.getElementById('phonetic').textContent = data.phonetic || data.phonetics[0]?.text || ''
+
     const audioUrl = data.phonetics.find(p => p.audio)?.audio
     const audioEl = document.getElementById('audio')
     if (audioUrl){
@@ -51,38 +69,98 @@ function displayResult(data) {
     } else {
         audioEl.classList.add('hidden')
     }
-}
-const saveButton = document.getElementById('save-btn')
-updatesaveButton(saveButton, data.word)
-saveButton.onclick = () => toggleFavorite(data.word)
 
-const meaningS = document.getElementById('meanings')
-meaningS.innerHTML = ''
-data.meanings.forEach(meaning => {
+const saveBtn = document.getElementById('save-btn')
+if(saveBtn){
+    updateSaveBtn(saveBtn, data.word)
+    saveBtn.onclick = () => toggleFavorite(data.word)
+}
+function updateSaveBtn(btn, word){
+    if(!btn) {
+        console.log('Save button not found')
+     return
+    }
+    const icon = btn.querySelector('.material-icons-round')
+    if(!icon) {
+        console.log('Icon span missing')
+        return
+    } 
+    if (favorites.includes(word)){
+        icon.textContent = 'bookmark'
+        btn.classList.add('active') 
+    } else{
+        icon.textContent = 'bookmark_border'
+        btn.classList.remove('active')
+    }
+
+}
+//if (saveBtn){
+    //updateSaveBtn(btn, data.word)
+  //  saveButton.onclick = () => toggleFavorite(data.word)
+//}
+
+const meaningsDiv = document.getElementById('meanings')
+console.log('2. meaningsDiv found:', meaningsDiv)
+
+if (!meaningsDiv){
+    console.error('ERROR: Missing html line')
+    showError('html error')
+    return
+}
+
+meaningsDiv.innerHTML = ''
+console.log('3. looping through meaning:', data.meanings)
+
+data.meanings?.forEach((meaning, index) => {
+    console.log(`4. processing meaning ${index}:`, meaning)
     const meaningEl = document.createElement('div')
     meaningEl.className = 'meaning'
+
+    const definitionsHTML = meaning.definitions?.slice(0, 3).map((def, i) => `
+    <div class="definition">
+    <p> ${i + 1}. ${def.definition}</p>
+    ${def.example? `<p class="example"> "${def.example}" </p>` : ''}
+    </div>
+    `).join('') 
+
+    const synonymsHTML = meaning.synonyms && meaning.synonyms.length?`
+    <div class ="chip-group">
+    <p class ="chip-title">Synonyms:</p>
+    ${meaning.synonyms.slice(0,5).map(s => `<span class="chip"> ${s}</span>`).join('')}
+    </div>
+    ` :''
+
     meaningEl.innerHTML = `
     <span class="part-of-speech">${meaning.partOfSpeech}</span>
+    ${definitionsHTML}
+    ${synonymsHTML}
+    
       ${meaning.definitions.slice(0, 3).map((def, i) =>`
         <div class = "definition">
         <p> ${i + 1}. ${def.definition} </p>
         ${def.example? `<p class="example"> "${def.example}"</p>` : ''}
         </div>
-        `).join('')}
-        ${meaning.synonyms.length? `
-            <div class="name-group">
-            ${meaning.synonyms.slice(0, 5).map(s => `<span class ="name"> ${s}</span>`).join('')}
+        `).join('')} || '<p> No definitions found </p>'
+        ${meaning.synonyms && meaning.synonyms.length? `
+            <div class="chip-group">
+            ${meaning.synonyms.slice(0, 5).map(s => `<span class ="chip"> ${s}</span>`).join('')}
             </div>
             ` : ''}
-           meaningS.appendChild(meaningS) 
+            `
+           meaningsDiv.appendChild(meaningEl) 
         })
-           document.getElementById('source").innerHTML = data.sourceUrls?
-           <small> Source: <a href="${data.sourceUrls[0]}" target ="_blank">${data.sourceUrls[0]}</a></small>` : ''
+        const sourceDiv = document.getElementById('source')
+           if (sourceDiv){
+            sourceDiv.innerHTML = data.sourceUrls?
+           `<small> Source: <a href="${data.sourceUrls[0]}" target ="_blank">${data.sourceUrls[0]}</a></small>` : ''
+           }
+           console.log('5. Done rendering')
            showResults()
-        })
+           
+    } 
         function toggleTheme(){
             const currentTheme = document.documentElement.getAttribute('data-theme')
-            const newTheme = current === 'dark'?'light' : 'dark'
+            const newTheme = currentTheme === 'dark'?'light' : 'dark'
             document.documentElement.setAttribute('data-theme', newTheme)
             themeToggle.querySelector('.material-icons-round').textContent =
             newTheme === 'dark'? 'light_mode' : 'dark_mode'
@@ -102,7 +180,7 @@ data.meanings.forEach(meaning => {
             }
             localStorage.setItem('wordly-favorites', JSON.stringify(favorites))
             renderFavorites()
-            updatesaveButton(document.getElementById('save-btn'), word)
+            updateSaveBtn(document.getElementById('save-btn'), word)
         }
         function renderFavorites(){
             favoritesList.innerHTML = ''
@@ -110,20 +188,22 @@ data.meanings.forEach(meaning => {
                 favoritesEmpty.classList.remove('hidden')
                 return
             }
-        favoritesEmpty.classList.remove('hidden')
+        favoritesEmpty.classList.add('hidden')
+
         favorites.forEach(word =>{
-            const name = document.createElement('button')
-            name.className = 'name'
-            name.textContent = word
-            name.onclick = () => {
+            const chip = document.createElement('button')
+            chip.className = 'chip'
+            chip.textContent = word
+            chip.onclick = () => {
                 wordInput.value = word
                 searchWord(word)
             }
-            favoritesList.appendChild(name)
-        })
+            favoritesList.appendChild(chip)
+        })}
+
         function updateSaveBtn(btn, word){
             const icon = btn.querySelector('.material-icons-round')
-            if (favorites.include(word)){
+            if (favorites.includes(word)){
                 icon.textContent = 'bookmark'
                 btn.classList.add('active')
             }else {
@@ -132,12 +212,21 @@ data.meanings.forEach(meaning => {
             }
         }
         function showLoader(){loader.classList.remove('hidden')}
-        function hideLoader(){loader.classList.add('hidden')}
+        function hideLoader(){
+            document.getElementById('loader').classList.add('hidden')
+            //loader.classList.add('hidden')
+            }
         function showError(message){
             errorText.textContent = message
             errorMessage.classList.remove('hidden')
         }
-        function hideError(){errorMessage.classList.add('hidden')}
-        function showResults(){resultSection.classList.remove('hidden')}
+        function hideError(){
+            document.getElementById('error-message').classList.add('hidden')
+            //errorMessage.classList.add('hidden')
+            }
+        function showResults(){
+            document.getElementById('result-section').classList.remove('hidden')
+            //resultSection.classList.remove('hidden')
+            }
         function hideResults(){resultSection.classList.add('hidden')}
-    }
+    
